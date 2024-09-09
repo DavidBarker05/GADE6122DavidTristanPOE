bce2660ecea7ee7e4458ef034445ef5596972f27
DavidBarker05 <davidbarker9611@gmail.com>
2024-09-09 16:40:49 +0200

    Fixed GameEngine

--- GADE6122DavidTristanPOE/GameEngine.cs
+++ GADE6122DavidTristanPOE/GameEngine.cs
@@ -8,27 +8,27 @@ namespace GADE6122DavidTristanPOE
 {
     internal class GameEngine
     {
-        private int levelAmt;
-        private int numLevel;
 
+        private Level currentLevel;
+        private int levelAmt;
         private Random rnd = new Random();
 
-        const int minSize = 10;
-        const int maxSize = 20;
+        const int MIN_SIZE = 10;
+        const int MAX_SIZE = 20;
 
 
         public GameEngine(int levelAmt)
         {
-            this.levelAmt = 0;
-            //?
-            return;
+            this.levelAmt = levelAmt;
+            int width = rnd.Next(MIN_SIZE, MAX_SIZE + 1);
+            int height = rnd.Next(MIN_SIZE, MAX_SIZE + 1);
+            currentLevel = new Level(width, height);
         }
 
         public override string ToString()
         {
-            return base.ToString();
+            return currentLevel.ToString();
         }
-        //?
-        //private int GameEngine { get { return (int)levelAmt; } }
+
     }
 }
