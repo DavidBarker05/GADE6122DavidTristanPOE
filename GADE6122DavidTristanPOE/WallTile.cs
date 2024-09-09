7f6ccdef654ce80bf0b0570bda7b000a6e9ea962
DavidBarker05 <davidbarker9611@gmail.com>
2024-09-09 16:19:35 +0200

    Formatted WallTile

--- GADE6122DavidTristanPOE/WallTile.cs
+++ GADE6122DavidTristanPOE/WallTile.cs
@@ -9,11 +9,13 @@ namespace GADE6122DavidTristanPOE
     //Q3.1
     internal class WallTile : Tile
     {
+
         public WallTile(Position position) : base(position)
         {
         }
 
         //?
         public override char Display => '█';
+
     }
 }
