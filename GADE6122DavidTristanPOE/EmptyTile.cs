57ea6acd9c3c269aae787d9b118c83eeba6f663f
DavidBarker05 <davidbarker9611@gmail.com>
2024-09-09 16:19:09 +0200

    Formatted EmptyTile

--- GADE6122DavidTristanPOE/EmptyTile.cs
+++ GADE6122DavidTristanPOE/EmptyTile.cs
@@ -3,10 +3,12 @@
     //Q2.3
     internal class EmptyTile : Tile
     {
+
         public EmptyTile(Position position) : base(position)
         {
         }
 
         public override char Display => '.';
+
     }
 }
