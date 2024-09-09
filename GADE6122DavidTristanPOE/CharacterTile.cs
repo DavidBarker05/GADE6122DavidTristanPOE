e1b2c7ccbdf0e768ef49ddc2f6892e5133c0d6c3
DavidBarker05 <davidbarker9611@gmail.com>
2024-09-09 17:09:14 +0200

    Added CharacterTile

---
+++ GADE6122DavidTristanPOE/CharacterTile.cs
@@ -0,0 +1,39 @@
+﻿namespace GADE6122DavidTristanPOE
+{
+    internal abstract class CharacterTile : Tile
+    {
+
+        private int hitPoints, maxHitPoints, attackPower;
+        private Tile[] vision;
+
+        public bool IsDead { get { return hitPoints == 0; } }
+
+        public CharacterTile(Position position, int hitPoints, int attackPower) : base(position)
+        {
+            this.hitPoints = hitPoints;
+            maxHitPoints = hitPoints;
+            this.attackPower = attackPower;
+            vision = new Tile[4];
+        }
+
+        public void UpdateVision(Level level)
+        {
+            vision[0] = level.Tiles[X, Y - 1];
+            vision[1] = level.Tiles[X + 1, Y];
+            vision[2] = level.Tiles[X, Y + 1];
+            vision[3] = level.Tiles[X - 1, Y];
+        }
+
+        public void TakeDamage(int damage)
+        {
+            hitPoints -= damage;
+            if (hitPoints < 0) hitPoints = 0;
+        }
+
+        public void Attack(CharacterTile characterTile)
+        {
+            characterTile.TakeDamage(attackPower);
+        }
+
+    }
+}
