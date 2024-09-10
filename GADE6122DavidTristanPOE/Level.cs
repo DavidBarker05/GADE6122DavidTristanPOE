d9c4a24556682d6dbf2bc7125e88efdf0c1b47df
DavidBarker05 <davidbarker9611@gmail.com>
2024-09-10 09:49:30 +0200

    Added hero generation

--- GADE6122DavidTristanPOE/Level.cs
+++ GADE6122DavidTristanPOE/Level.cs
@@ -1,4 +1,6 @@
-﻿namespace GADE6122DavidTristanPOE
+﻿using System;
+
+namespace GADE6122DavidTristanPOE
 {
     //Q2.4
     internal class Level
@@ -6,20 +8,27 @@
         public enum TileType
         {
             EmptyTile,
-            WallTile
+            WallTile,
+            HeroTile
         }
 
         private Tile[,] tiles;
         private int width, height;
+        private HeroTile hero;
+
+        public HeroTile Hero { get { return hero; } }
 
         public Tile[,] Tiles { get {  return tiles; } set { tiles = value; } }
 
-        public Level(int width, int height)
+        public Level(int width, int height, HeroTile heroTile = null)
         {
             this.width = width;
             this.height = height;
             tiles = new Tile[width, height];
             InitialiseTiles();
+            Position heroPos = GetRandomEmptyPosition();
+            if (heroTile == null) CreateTile(TileType.HeroTile, heroPos);
+            else tiles[heroPos.X, heroPos.Y] = heroTile;
         }
 
         private Tile CreateTile(TileType tileType, Position position)
@@ -33,6 +42,9 @@
                     break;
                 case TileType.WallTile: 
                     tile = new WallTile(position);
+                    break;
+                case TileType.HeroTile:
+                    tile = new HeroTile(position);
                     break;
             }
 
@@ -56,6 +68,18 @@
                     else CreateTile(TileType.EmptyTile, x, y);
                 }
             }
+        }
+
+        private Position GetRandomEmptyPosition()
+        {
+            Random r = new Random();
+            int x, y;
+            do
+            {
+                x = r.Next(0, width);
+                y = r.Next(0, height);
+            } while (!(tiles[x, y] is EmptyTile));
+            return new Position(x, y);
         }
 
         public override string ToString()
